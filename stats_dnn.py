2baff439dcb567d16cc50df47d9c17bb43d08d08
Giulio Sidoretti <giuliosidoretti@gmail.com>
2025-01-07 12:26:48 +0000

    stats printing improved with all datasets

--- stats_dnn.py
+++ stats_dnn.py
@@ -5,7 +5,8 @@ from sklearn import metrics
 
 TEST = "traces_out/malware_web_dirty_test.csv"
 TEST_CLEAN = "traces_out/malware_web_test.csv"
-MODEL_CHECKPOINT_PATH = "models/malware_web_dirty_mixed_cleaned_dnn.keras"
+MODEL_CHECKPOINT_PATH_DIRTY = "models/malware_web_dirty_mixed_cleaned_dnn.keras"
+MODEL_CHECKPOINT_PATH = "models/malware_web_mixed_cleaned_dnn.keras"
 
 
 x_test = pd.read_csv(TEST, header=None)
@@ -23,6 +24,27 @@ print(f"shape of y_test: {y_test_clean.shape}")
 
 # load best model from file
 model = keras.models.load_model(MODEL_CHECKPOINT_PATH)
+
+# evaluate model
+score = model.evaluate(x_test_clean, y_test_clean, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+print(model.metrics_names)
+# f1 score
+y_pred = model.predict(x_test_clean, batch_size=2048, verbose=1)
+y_pred_bool = np.argmax(y_pred, axis=1)
+
+y_test_bool = np.argmax(y_test_clean.values, axis=1)
+print(f"f1: {metrics.f1_score(y_pred_bool, y_test_bool, average=None)}")
+print(f"avg: {metrics.f1_score(y_test_bool, y_pred_bool)}")
+print(f"precision: {metrics.precision_score(y_test_bool, y_pred_bool, average=None)}")
+print(f"avg: {metrics.precision_score(y_test_bool, y_pred_bool)}")
+print(f"recall: {metrics.recall_score(y_test_bool, y_pred_bool, average=None)}")
+print(f"avg: {metrics.recall_score(y_test_bool, y_pred_bool)}")
+
+print("######\n######\n######\nReevaluating on dirty DNN\n######\n######\n######")
+# load best model from file
+model = keras.models.load_model(MODEL_CHECKPOINT_PATH_DIRTY)
 
 # evaluate model
 score = model.evaluate(x_test, y_test, verbose=0)
